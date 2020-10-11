06449ba91b86de66ffbe0a46cc160cc7e7f0ef82
jomedinas7 <jmedinas@miners.utep.edu>
2020-10-10 21:37:14 -0600

    red and green blinking at same time

--- demos/blink_demo/stateMachines.c
+++ demos/blink_demo/stateMachines.c
@@ -21,12 +21,26 @@ char toggle_red()		/* always toggle! */
 
 char toggle_green()	/* only toggle green if red is on!  */
 {
-  char changed = 0;
+  /* char changed = 0;
   if (red_on) {
     green_on ^= 1;
     changed = 1;
   }
-  return changed;
+  */
+  static char state = 0;
+
+  switch (state) {
+  case 0:
+    green_on = 1;
+    state = 1;
+    break;
+  case 1:
+    green_on = 0;
+    state = 0;
+    break;
+  }
+  
+  return 1;
 }
 
 
@@ -36,8 +50,8 @@ void state_advance()		/* alternate between toggling red & green */
 
   static enum {R=0, G=1} color = G;
   switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
+    /* case R: changed = toggle_red(); color = G; break;*/
+  case G: changed = toggle_green(); toggle_red(); color = G; break;
   }
 
   led_changed = changed;
